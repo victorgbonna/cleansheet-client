a4177ba1077bb38ff71a8775438531383e5fefe4
Victor Ogbonna <victorgbonna@gmail.com>
2025-06-17 09:28:08 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        bgpc: { max: "2400px" },
+        pc: { max: "1800px" },
+        tablet: { max: "900px" },
+        phone: { max: "540px" },
+      },
     },
   },
   plugins: [],
